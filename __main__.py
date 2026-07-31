8e90295ca1cb902cad1d1d599bf663585898915a
SandroBolkvadze <sbolk23@freeuni.edu.ge>
2026-07-31 16:22:28 +0400

    update

--- __main__.py
+++ __main__.py
@@ -1,2 +1,21 @@
+from core.engine import Engine
+
 if __name__ == "__main__":
-    pass
+    hdl = """
+        /* dasd asd asd asdasd asd 
+            dsadasdasd
+        */
+        // dasd sad asd asd asd asdsa
+        abcd
+        // dasda sdasd asd as dsadadsad
+        zzz
+    """
+
+    engine = Engine(hdl)
+    engine.advance()
+    print(hdl[engine.index])
+    engine.index += 4
+    engine.advance()
+    print(hdl[engine.index])
+
+
--- core/engine.py
+++ core/engine.py
@@ -3,29 +3,70 @@ from __future__ import annotations
 class Engine:
     def __init__(self, hdl: str) -> None:
         self.hdl = hdl
-        self.index = 0
 
-    def advance(self) -> None:
-        while self.index < len(self.hdl):
-            if self.hdl[self.index] in [" ", "\n"]:
-                self.index += 1
-            if self.hdl[self.index] == "/":
-                self.parse_comment()
+    def peek_advance(self) -> int:
 
-    def parse_comment_to_end_line(self) -> None:
-        assert self.hdl[self.index: self.index + 2] == "//"
-        self.index = self.hdl.find("\n", self.index + 2) + 1
+        pass
 
-    def parse_comment_until_close(self) -> None:
-        assert self.hdl[self.index: self.index + 2] == "/*"
-        self.index = self.hdl.find("*/", self.index + 2) + 2
+    def advance(self, index: int) -> int:
+        while index < len(self.hdl):
+            if self.hdl[index] in [" ", "\n"]:
+                index += 1
+            elif self.hdl[index] == "/":
+                self.consume_comment(index)
+            else:
+                break
+        return index
 
-    def parse_comment(self) -> None:
-        assert self.hdl[self.index] == "/"
+    def consume_comment_to_end_line(self, index: int) -> int:
+        if self.hdl.find("//", index) != index:
+            return index
+        tmp = self.hdl.find("\n", index + 2)
+        if tmp == -1:
+            return index
+        return tmp + 1
 
-        if self.hdl[self.index + 1] == "/":
-            self.parse_comment_to_end_line()
+    def consume_comment_until_close(self, index: int) -> int:
+        if self.hdl.find("/*", index) != index:
+            return index
+        tmp = self.hdl.find("*/", index + 2)
+        if tmp == -1:
+            return index
+        return tmp + 2
 
-        if self.hdl[self.index + 1] == "*":
-            self.parse_comment_until_close()
+    def consume_comment(self, index: int) -> int:
+        if index + 1 >= len(self.hdl) or self.hdl[index] != "/":
+            return index
+
+        if self.hdl[index + 1] == "/":
+            return self.consume_comment_to_end_line(index)
+
+        if self.hdl[index + 1] == "*":
+            return self.consume_comment_until_close(index)
+
+        return index
+
+    def consume_keyword(self, index: int, keyword: str) -> int:
+        if self.hdl[index: min(index + len(keyword), len(self.hdl))] != keyword:
+            return index
+
+        if self.advance(index + len(keyword)) == index + len(keyword):
+            return index
+
+        return index + len(keyword)
+
+    def consume_token(self, index: int) -> int:
+        while index < len(self.hdl) and (self.hdl[index].isalpha() or self.hdl[index].isnumeric()):
+            index += 1
+        return index
+
+    def build(self) -> None:
+        self.parse_declaration(self.advance(0))
+
+    def parse_declaration(self, index: int) -> None:
+        self.consume_keyword(index, "CHIP")
+        chip_name = self.consume_token()
+
+
+
 
